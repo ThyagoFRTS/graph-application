d67841b04870a6d9050c7b12c5576de5c3c9d95d
ThyagoFRTS <kzkr.thyago@gmail.com>
2023-05-02 20:22:58 -0300

    refactor: componentizes ui elements

---
+++ src/frontend/components.rs
@@ -0,0 +1,47 @@
+use gtk::glib::clone;
+use gtk::{prelude::*, MessageDialog, DialogFlags};
+use gtk::{
+    Button,
+    Orientation, 
+    Label,
+    DropDown,
+    Separator
+};
+
+pub fn build_separator() -> Separator {
+    Separator::builder()
+        .orientation(Orientation::Horizontal)
+        .height_request(2)
+        .margin_start(12)
+        .margin_end(12)
+        .build()
+}
+
+pub fn build_label(title: &str) -> Label {
+    Label::builder()
+        .label(title)
+        .margin_top(12)
+        .margin_bottom(12)
+        .margin_start(12)
+        .margin_end(12)
+        .build()
+}
+
+pub fn build_button(title: &str) -> Button {
+    Button::builder()
+        .label(title)
+        .margin_top(12)
+        .margin_bottom(12)
+        .margin_start(12)
+        .margin_end(12)
+        .build()
+}
+
+pub fn build_dropdown() -> DropDown {
+    DropDown::builder()
+        .margin_top(12)
+        .margin_bottom(12)
+        .margin_start(12)
+        .margin_end(12)
+        .build()
+}
