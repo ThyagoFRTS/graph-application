0a577824d5f2c6d8a36d53b116d747b48a69b2c4
ThyagoFRTS <kzkr.thyago@gmail.com>
2023-05-02 01:44:51 -0300

    feat: implements dropdown

--- src/frontend/interface.rs
+++ src/frontend/interface.rs
@@ -1,6 +1,5 @@
 use gtk::glib::clone;
 use gtk::prelude::*;
-//use gtk::{glib, Application, ApplicationWindow};
 use gtk::{
     glib,
     Application,
@@ -15,6 +14,8 @@ use gtk::{
     Label,
     Frame,
     DropDown,
+    StringList,
+    Expression,
     Separator, 
     ResponseType
 };
@@ -57,9 +58,25 @@ pub fn build_ui(app: &Application) {
         .sensitive(false)
         .build();
 
-    let vertex_dropdown = DropDown::builder()
+    
+
+    //let vertex_list1_dropdown = DropDown::new(Some(model), gtk::Expression::NONE);
+    let vertex_list1_dropdown = DropDown::builder()
+        //.expression(gtk::Expression::NONE)
+        .margin_top(12)
+        .margin_bottom(12)
         .margin_start(12)
         .margin_end(12)
+        .sensitive(false)
+        .build();
+
+    let vertex_list2_dropdown = DropDown::builder()
+        //.expression(gtk::Expression::NONE)
+        .margin_top(12)
+        .margin_bottom(12)
+        .margin_start(12)
+        .margin_end(12)
+        .sensitive(false)
         .build();
 
     let output_frame = Frame::builder()
@@ -77,12 +94,14 @@ pub fn build_ui(app: &Application) {
 
     let vertex_layout = Box::new(Orientation::Horizontal, 0);
     vertex_layout.set_halign(Align::Center);
-    vertex_layout.append(&vertex_dropdown);
+    vertex_layout.append(&vertex_list1_dropdown);
+    vertex_layout.append(&vertex_list2_dropdown);
     //vertex_layout.append(&tree_detect_button);
 
     let main_layout = Box::new(Orientation::Vertical, 0);
     main_layout.append(&buttons_layout);
     main_layout.append(&path_file_label);
+    main_layout.append(&vertex_layout);
     main_layout.append(&horiozontal_line);
     //main_layout.append(&horiozontal_line);
     main_layout.append(&output_frame);
@@ -90,7 +109,9 @@ pub fn build_ui(app: &Application) {
     load_file_button.connect_clicked(clone!(
         @weak main_window, 
         @weak path_file_label,
-        @weak tree_detect_button
+        @weak tree_detect_button,
+        @weak vertex_list1_dropdown,
+        @weak vertex_list2_dropdown
           => move |_button| {
         let file_explorer = FileChooserDialog::new(
             Some("Selecione o arquivo de texto"),
@@ -108,13 +129,36 @@ pub fn build_ui(app: &Application) {
 
         file_explorer.connect_response(clone!(
             @weak path_file_label,
+            @weak vertex_list1_dropdown,
+            @weak vertex_list2_dropdown,
             @weak tree_detect_button => move |file_explorer, response| {
             if response == ResponseType::Ok {
                 if let Some(file) = file_explorer.file() {
                     if let Some(path) = file.path() {
                         println!("Caminho do arquivo selecionado: {:?}", path.as_os_str());
                         let mut g = Graph::new();
-                        g.load_from_file(path.as_os_str().to_str().unwrap());
+
+                        let path = path.as_os_str().to_str().unwrap();
+                        g.load_from_file(path);
+                        //.iter().map(|&x| format!("v{}", x)).collect();
+                        path_file_label.set_text(path);
+
+                        let model1 = StringList::new(&[]);
+
+                        for vertex in g.get_vertex_list(){
+                            let opt = &("v".to_owned() +vertex.to_string().as_str());
+                            model1.append(opt);
+                        }
+
+                        //println!("{:?}",list);
+
+
+                        vertex_list1_dropdown.set_model(Some(&model1));
+                        vertex_list1_dropdown.set_expression(Expression::NONE);
+                        vertex_list1_dropdown.set_sensitive(true);
+                        vertex_list2_dropdown.set_model(Some(&model1));
+                        vertex_list2_dropdown.set_expression(Expression::NONE);
+                        vertex_list2_dropdown.set_sensitive(true);
                         g.print_graph();
                         tree_detect_button.set_sensitive(true);
                     }
