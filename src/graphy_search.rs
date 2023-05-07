43e221eed379473ed340d9a0a480142b2e4b2e5b
ThyagoFRTS <kzkr.thyago@gmail.com>
2023-05-07 18:26:27 -0300

    feat: create visit all edges function

--- src/graphy_search.rs
+++ src/graphy_search.rs
@@ -1,4 +1,14 @@
 
+fn get_neighbors(arr: &mut Vec<i8>) -> Vec<usize>{
+    let mut indexes: Vec<usize> = Vec::new();
+    for (i, &val) in arr.iter().enumerate() {
+        if val == 1 {
+            indexes.push(i);
+        }
+    }
+    indexes
+}
+
 fn deep_search_in(vertex: usize, stack: &mut Vec<bool>, graph_line:  &mut Vec<Vec<i8>>, path: &mut Vec<(usize, usize)>,pos_ordering: bool){
     stack[vertex] = true;
     let neighbors = get_neighbors(&mut graph_line[vertex]);
@@ -31,13 +41,24 @@ pub fn deep_search(graph: Vec<Vec<i8>>) -> Vec<(usize, usize)>{
     }
     path
 }
-/* 
-pub fn vist_all_edges(graph: Vec<Vec<i8>>, start: usize) {
-    let mut graph = graph;
-    let mut stack: Vec<(usize,usize)> = vec![];
 
+pub fn visit_all_edges(graph: Vec<Vec<i8>>, is_digraph: bool) -> Vec<(usize, usize)>{
+    let mut temp_graph = graph;
+    let mut path: Vec<(usize, usize)> = Vec::new();
+
+    for i in 0..temp_graph.len() {
+        for j in 0..temp_graph[i].len() {
+            if temp_graph[i][j] == 1 {
+                path.push((i,j));
+                if !is_digraph {
+                    temp_graph[j][i] = 0;
+                }
+            }
+        }
+    }
+    path
 }
-*/
+
 pub fn lenght_search(graph: Vec<Vec<i8>>, vertex: usize){
     let mut graph = graph;
     let total_vertex = graph.len();
